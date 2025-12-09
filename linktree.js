d602c071503484ed5863c4a53c20913002a93c90
aryank831 <155168497+aryank831@users.noreply.github.com>
2025-12-09 22:42:34 +0530

    Delete linktree.js

--- linktree.js
+++
@@ -1,8 +0,0 @@
-// This script can be used to add dynamic features or handle link clicks if needed
-
-document.querySelectorAll('.link').forEach(link => {
-    link.addEventListener('click', () => {
-      alert(`You clicked: ${link.textContent}`);
-    });
-  });
-  
